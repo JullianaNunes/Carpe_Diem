c5e7dfe51a774bb4f81d7dacb1374303d0d8b397
Julliana Nunes <julliananunesdf@hotmail.com>
2020-06-02 21:39:02 -0300

    Atualização da tela sobre

--- mysite2/snipp/forms.py
+++ mysite2/snipp/forms.py
@@ -5,3 +5,9 @@ class Email_save(forms.ModelForm):
     class Meta:
         model = ModelEmail
         fields = ('email',)  
+
+class Contato_save(forms.ModelForm):
+    class Meta: 
+        model = Contato
+        fields = ('nome', 'email', 'assunto', 'mensagem')
+        email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email'}))
